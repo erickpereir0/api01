3c156c8d7f5e63d95118d2f101d74b48f5305a9f
erickpereir0 <erickpereira37140@gmail.com>
2024-08-07 17:07:59 -0300

    finalizando controller e services

--- demo/src/main/java/controller/UsersController.java
+++ demo/src/main/java/controller/UsersController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import service.Usersservices;
 
 import java.net.URI;
+import java.util.List;
 
 
 @RestController
@@ -26,6 +27,31 @@ public class UsersController {
     }
 
     @GetMapping
-    public ResponseEntity<Users> getUsers() {
+    public ResponseEntity<List<Users>> getAllUsers() {
+        var users = usersservice.getAllUsers();
+
+        return (ResponseEntity<List<Users>>) ResponseEntity.ok(users);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Users> getUserById(@PathVariable("id") Long id)  {
+        var users = usersservice.getUserById(id);
+
+        return (ResponseEntity<Users>) ResponseEntity.ok(users);
+    }
+
+    @PutMapping
+    public ResponseEntity<Users> updateUser(@PathVariable("id") Long id, @RequestBody UpdateUsersDto updateUsersDto) {
+        usersservice.updateUserById(id, updateUsersDto);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Users> deleteUser(@PathVariable("id") Long id)  {
+        usersservice.deleteUserById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- demo/src/main/java/service/Usersservices.java
+++ demo/src/main/java/service/Usersservices.java
@@ -1,6 +1,7 @@
 package service;
 
 import controller.CreateUsersDto;
+import controller.UpdateUsersDto;
 import model.Users;
 import org.springframework.stereotype.Service;
 import repository.UsersRepository;
@@ -35,7 +36,25 @@ public class Usersservices {
         return usersRepository.findById(id).get();
     }
 
-    public Users updateUserById(Long id, CreateUsersDto createUsersDto) {
+    public Users updateUserById(Long id, UpdateUsersDto updateUsersDto ) {
 
+        var userEntity = usersRepository.findById(id);
+
+        if (userEntity.isPresent()) {
+            var user = userEntity.get();
+
+            if (updateUsersDto.username() != null) {
+             user.setUsername(updateUsersDto.username());
+            }
+
+            if (updateUsersDto.password() != null) {
+                user.setPassword(updateUsersDto.password());
+            }
+        }
+        return getUserById(id);
+    }
+
+    public void deleteUserById(Long id) {
+        usersRepository.deleteById(id);
     }
 }
